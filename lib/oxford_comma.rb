6fbf85f047aedeb95308a2459b03188524d28342
Aaron Chu <aaronchu357@gmail.com>
2019-04-04 21:50:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,11 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length == 3
-    array.join(", ")
+    last_element = array.pop
+    and_string = "and "
+    final = and_string.concat(last_element)
+    array << final
+    return array.join(", ")
   else array.length > 3
     array.join(", ")
   end
